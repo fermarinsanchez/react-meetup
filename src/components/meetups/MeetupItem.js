eab4ec07972473b8cdd4928a5b19a190363a48be
fermarinsanchez <fermarinsanchez@yahoo.es>
2023-04-21 00:14:56 +0200

    feat: Favorites feature
    Add favs meetups to favorites section. When user add to favorites, in all meetup section change the button to a favorite message. Meetup items change button and onClick function depends where are showing (add or remove)
    
    Refs: #9

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,9 +1,13 @@
 
+import { useDispatch } from "react-redux";
+import { addFavorite, removeFavorite } from "../../actions/index";
 import classes from "./MeetupItem.module.css";
 import Card from "../ui/Card";
 
-export default function MeetupItem({data}) {
- 
+export default function MeetupItem({data, section, isFavorite}) {
+
+  const dispatch = useDispatch()
+  const isFavoriteSection = section === 'favs'
   return (
     <li className={classes.item} data-test='meet-up-item'>
       <Card>
@@ -16,7 +20,12 @@ export default function MeetupItem({data}) {
           <p>{data.description}</p>
         </div>
         <div className={classes.actions}>
-          <button>Add to favorites</button>
+          {isFavorite &&  <div className={classes.fav} disabled>FAVORITE!</div>}
+          {isFavoriteSection ? 
+            <button onClick={() => dispatch(removeFavorite(data))}>Remove from favorites</button>
+            :
+            isFavorite ?  null : <button onClick={() => dispatch(addFavorite(data))}>Add to favorites</button>
+            }
         </div>
       </Card>
     </li>
